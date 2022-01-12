d0dbdf2dddc08a8de07a2ecb01cf2e290aa3faf1
Pete Cornish <outofcoffee@gmail.com>
2022-01-12 12:11:24 +0000

    refactor: changes Java discovery to prefer PATH before JAVA_HOME.

--- engine/jvm/java.go
+++ engine/jvm/java.go
@@ -13,6 +13,12 @@ import (
 // GetJavaCmdPath finds the best candidate for the 'java' command, searching
 // the environment as well as using well-known OS-specific mechanisms.
 func GetJavaCmdPath() (string, error) {
+	// search for 'java' in the PATH
+	javaPath, err := exec.LookPath("java")
+	if err != nil {
+		logrus.Tracef("could not find 'java' in PATH: %s", err)
+	}
+
 	var binaryPathSuffix string
 	if runtime.GOOS == "Windows" {
 		binaryPathSuffix = ".exe"
@@ -20,30 +26,31 @@ func GetJavaCmdPath() (string, error) {
 		binaryPathSuffix = ""
 	}
 
-	// prefer JAVA_HOME environment variable
-	if javaHomeEnv, found := os.LookupEnv("JAVA_HOME"); found {
-		return filepath.Join(javaHomeEnv, "/bin/java"+binaryPathSuffix), nil
+	if javaPath == "" {
+		// check JAVA_HOME environment variable
+		if javaHomeEnv, found := os.LookupEnv("JAVA_HOME"); found {
+			javaPath = filepath.Join(javaHomeEnv, "/bin/java"+binaryPathSuffix)
+		}
 	}
 
-	if runtime.GOOS == "darwin" {
+	if javaPath == "" && runtime.GOOS == "darwin" {
 		command, stdout := exec.Command("/usr/libexec/java_home"), new(strings.Builder)
 		command.Stdout = stdout
 		err := command.Run()
 		if err != nil {
-			return "", fmt.Errorf("error determining JAVA_HOME: %v", err)
+			return "", fmt.Errorf("error determining JAVA_HOME: %s", err)
 		}
 		if command.ProcessState.Success() {
-			return filepath.Join(strings.TrimSpace(stdout.String()), "/bin/java"+binaryPathSuffix), nil
+			javaPath = filepath.Join(strings.TrimSpace(stdout.String()), "/bin/java"+binaryPathSuffix)
 		} else {
 			return "", fmt.Errorf("failed to determine JAVA_HOME using libexec")
 		}
 	}
 
-	// search for 'java' in the PATH
-	javaPath, err := exec.LookPath("java")
-	if err != nil {
-		return "", fmt.Errorf("could not find 'java' in PATH: %v", err)
+	if javaPath == "" {
+		return "", fmt.Errorf("failed to determine Java path - consider setting JAVA_HOME or updating PATH")
 	}
+
 	logrus.Tracef("using java: %v", javaPath)
 	return javaPath, nil
 }
