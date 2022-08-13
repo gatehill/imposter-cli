75375c33dbdbe9f2792d1e2038082fe382d56201
Pete Cornish <outofcoffee@gmail.com>
2022-08-14 00:06:21 +0100

    feat: adds reverse proxy.

---
+++ cmd/proxy.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2022 Pete Cornish <outofcoffee@gmail.com>
+
+Licensed under the Apache License, Proxy 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"gatehill.io/imposter/proxy"
+	"github.com/spf13/cobra"
+	"net/http"
+	"os"
+)
+
+var proxyFlags = struct {
+	port      int
+	outputDir string
+}{}
+
+// proxyCmd represents the up command
+var proxyCmd = &cobra.Command{
+	Use:   "proxy [URL]",
+	Short: "Proxy an endpoint and record HTTP exchanges",
+	Long:  `Proxies an endpoint and records HTTP exchanges to file, in Imposter format.`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		upstream := args[0]
+		var outputDir string
+		if proxyFlags.outputDir != "" {
+			outputDir = proxyFlags.outputDir
+		} else {
+			workingDir, err := os.Getwd()
+			if err != nil {
+				panic(err)
+			}
+			outputDir = workingDir
+		}
+		proxyUpstream(upstream, proxyFlags.port, outputDir)
+	},
+}
+
+func init() {
+	proxyCmd.Flags().IntVarP(&proxyFlags.port, "port", "p", 8080, "Port on which to listen")
+	proxyCmd.Flags().StringVarP(&proxyFlags.outputDir, "output-dir", "o", "", "Directory in which HTTP exchanges are recorded (default: current working directory)")
+	rootCmd.AddCommand(proxyCmd)
+}
+
+func proxyUpstream(upstream string, port int, dir string) {
+	http.HandleFunc("/system/status", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = fmt.Fprintf(writer, "ok\n")
+	})
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		proxy.Handle(upstream, writer, request, dir)
+	})
+	logger.Infof("starting proxy for upstream %s on port %v", upstream, port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ proxy/proxy.go
@@ -0,0 +1,144 @@
+/*
+Copyright © 2022 Pete Cornish <outofcoffee@gmail.com>
+
+Licensed under the Apache License, Proxy 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"bytes"
+	"fmt"
+	"gatehill.io/imposter/logging"
+	"gatehill.io/imposter/stringutil"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+// Hop-by-hop headers. These are removed when sent to the upstream.
+// See "13.5.1 End-to-end and Hop-by-hop Headers" in http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
+var hopHeaders = []string{
+	"Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"TE",
+	"Trailers",
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+var logger = logging.GetLogger()
+
+func Handle(upstream string, w http.ResponseWriter, req *http.Request, outputDir string) {
+	startTime := time.Now()
+
+	client := req.RemoteAddr
+	logger.Debugf("received request %v %v from client %v", req.Method, req.URL, client)
+
+	path, clientReqHeaders, requestBody, err := parseRequest(req)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	statusCode, responseBody, upstreamRespHeaders, err := forward(upstream, req.Method, path, clientReqHeaders, requestBody)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(http.StatusBadGateway)
+		return
+	}
+
+	err = sendResponse(w, upstreamRespHeaders, statusCode, responseBody, client)
+	if err != nil {
+		logger.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	elapsed := time.Since(startTime)
+	logger.Infof("proxied %s %v to upstream [status: %v, body %v bytes] for client %v in %v", req.Method, req.URL, statusCode, len(*responseBody), client, elapsed)
+}
+
+func parseRequest(req *http.Request) (path string, headers *http.Header, body *[]byte, err error) {
+	defer req.Body.Close()
+	requestBody, err := io.ReadAll(req.Body)
+	if err != nil {
+		return "", nil, nil, fmt.Errorf("error parsing request body: %v", err)
+	}
+	return req.URL.Path, &req.Header, &requestBody, nil
+}
+
+func forward(
+	upstream string,
+	httpMethod string,
+	path string,
+	clientRequestHeaders *http.Header,
+	requestBody *[]byte,
+) (statusCode int, responseBody *[]byte, upstreamRespHeaders *http.Header, err error) {
+	logger.Debugf("invoking upstream %s with %s %s [body: %v bytes]", upstream, httpMethod, path, len(*requestBody))
+
+	upstreamUrl, err := url.JoinPath(upstream, path)
+	if err != nil {
+		return 0, nil, nil, fmt.Errorf("failed to build upstream URL: %v", err)
+	}
+	logger.Tracef("upstream url: %s", upstreamUrl)
+
+	req, err := http.NewRequest(httpMethod, upstreamUrl, bytes.NewReader(*requestBody))
+	upstreamReqHeaders := req.Header
+	copyHeaders(clientRequestHeaders, &upstreamReqHeaders)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+	defer resp.Body.Close()
+
+	var respBody []byte
+	if resp.Body != nil {
+		respBody, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return 0, nil, nil, fmt.Errorf("error parsing request body: %v", err)
+		}
+	}
+	logger.Debugf("upstream responded to %s %s with status %d [body %v bytes]", httpMethod, upstreamUrl, resp.StatusCode, len(respBody))
+	return resp.StatusCode, &respBody, &resp.Header, nil
+}
+
+func sendResponse(w http.ResponseWriter, headers *http.Header, statusCode int, body *[]byte, client string) (err error) {
+	clientRespHeaders := w.Header()
+	copyHeaders(headers, &clientRespHeaders)
+	_, err = w.Write(*body)
+	if err != nil {
+		return fmt.Errorf("error writing response: %v", err)
+	}
+
+	logger.Debugf("wrote response [status: %v, body %v bytes] to client %v", statusCode, len(*body), client)
+	return nil
+}
+
+// copyHeaders copies all headers from source to destination, unless the name
+// of the header is a hop-by-hop header.
+func copyHeaders(source *http.Header, destination *http.Header) {
+	for headerName, headerValues := range *source {
+		if !stringutil.Contains(hopHeaders, headerName) {
+			for _, headerValue := range headerValues {
+				destination.Add(headerName, headerValue)
+			}
+		}
+	}
+}
