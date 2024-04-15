98129f2df97919d4eaafedb432d1c54eae42ff63
Pete Cornish <outofcoffee@gmail.com>
2024-04-15 19:51:12 +0100

    fix: don't quieten the default logger if trace is enabled.

--- logging/logger.go
+++ logging/logger.go
@@ -5,14 +5,16 @@ import "github.com/sirupsen/logrus"
 var logger = logrus.New()
 
 func SetLogLevel(lvl string) {
-	// quieten the default logger
-	logrus.SetLevel(logrus.InfoLevel)
-
 	ll, err := logrus.ParseLevel(lvl)
 	if err != nil {
 		ll = logrus.DebugLevel
 	}
 	logger.SetLevel(ll)
+
+	if ll != logrus.TraceLevel {
+		// quieten the default logger
+		logrus.SetLevel(logrus.InfoLevel)
+	}
 }
 
 // GetLogger returns the configured logger.
